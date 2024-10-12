abbf8702d0b8a7a304a20077492be9bd8c562906
miurakouta <mk.craft.website@gmail.com>
2024-10-12 17:34:18 +0900

    page-contactのクラス名修正

--- page-contact.php
+++ page-contact.php
@@ -2,53 +2,52 @@
 <main>
 
         <section id="contact" class="content">
-        <!-- <div class="contact__container"> -->
-                <div class="contact--title">
+                <div class="contact__title">
                     <h1 class="level1__heading--en">CONTACT</h1>
                     <p class="level1__heading--ja">問い合わせ</p>
-                    <div class="contact--img-wrapper">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/contact1.png" alt="問い合わせに答える人" class="contact--img" width="400" height="200" decoding="async">
+                    <div class="contact__imgWrap">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/contact1.png" alt="問い合わせに答える人"  width="400" height="200" decoding="async">
                     </div>
-                    <!-- /.contact--img-wrapper -->
+                    <!-- /.contact__imgWrap -->
                 </div>
-                <!-- /.contact--title -->
-                <div class="contact__wrapper">
-                    <div class="confirmImgWrapper">
-                        <div class="contact__situation confirm confirm2 confirm3">
+                <!-- /.contact__title -->
+                <div class="contact__container">
+                    <div class="contact__WrapProgress">
+                        <div class="contact__boxProgress confirm confirm2 confirm3">
                             <picture>
                                 <source srcset="<?php echo get_template_directory_uri(); ?>/images/contact-sp1.png" media="(max-width: 767px)" type="image/png">
                                 <img src="<?php echo get_template_directory_uri(); ?>/images/contact-pc1.png" alt="内容を入力" class="contact__top-thumb" width="400" height="200" decoding="async">
                             </picture>
                         </div>
-                        <!-- /.contact__situation -->
-                        <div class="contact__situation confirm confirm2 confirm3">
+                        <!-- /.contact__boxProgress -->
+                        <div class="contact__boxProgress confirm confirm2 confirm3">
                             <picture>
                                 <source srcset="<?php echo get_template_directory_uri(); ?>/images/contact-sp2.png" media="(max-width: 767px)" type="image/png">
                                 <img src="<?php echo get_template_directory_uri(); ?>/images/contact-pc2.png" alt="内容を確認" class="contact__top-thumb" width="400" height="200" decoding="async">
                             </picture>
                         </div>
-                        <!-- /.contact__situation -->
-                        <div class="contact__situation confirm confirm2 confirm3">
+                        <!-- /.contact__boxProgress -->
+                        <div class="contact__boxProgress confirm confirm2 confirm3">
                             <picture>
                                 <source srcset="<?php echo get_template_directory_uri(); ?>/images/contact-sp3.png" media="(max-width: 767px)" type="image/png">
                                 <img src="<?php echo get_template_directory_uri(); ?>/images/contact-pc3.png" alt="送信完了" class="contact__top-thumb" width="400" height="200" decoding="async">
                             </picture>
                         </div>
-                        <!-- /.contact__situation -->
+                        <!-- /.contact__boxProgress -->
                     </div>
-                    <!-- /.confirmImgWrapper -->
-                    <div class="contact__container--form">
+                    <!-- /.contact__WrapProgress -->
+                    <div class="contact__containerForm">
                         <?php echo do_shortcode('[contact-form-7 id="8617d68" title="コンタクトフォーム 1"]'); ?>
                     </div>
-                    <!-- /.contact__container--form -->
-                    <div class="contact__text--recapcha">
+                    <!-- /.contact__containerForm -->
+                    <div class="contact__textRecapcha">
                         This site is protected by reCAPTCHA and the Google
                         <a href="https://policies.google.com/privacy">Privacy Policy</a> and
                         <a href="https://policies.google.com/terms">Terms of Service</a> apply.
                     </div>
-                    <!-- /.contact__text--recapcha -->
+                    <!-- /.contact__textRecapcha -->
                 </div>
-                <!-- /.contact__wrapper -->
+                <!-- /.contact__container -->
         </section>
 
 </main>
