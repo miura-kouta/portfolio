5045bf652bcf1fdc95319e5b73365c65b888ec58
miura-kouta <mk.craft.website@gmail.com>
2024-10-18 00:46:21 +0900

    試しで入力したものを削除

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<!-- 試しで入力 -->
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
